e7ce982d9532826a62270b9d00889ca9d9fb53f6
crisspina <56394860+crisspina@users.noreply.github.com>
2021-03-06 14:05:05 +0700

    [Add] first commit add hw_6

---
+++ 62130500124_groupwork_6/main.js
@@ -0,0 +1,75 @@
+
+const constraints = {
+    firstname: {
+        presence: true,
+        format:{
+            pattern: "[a-z]+",
+            message: "can only contain a-z "
+        }
+    },
+    lastname: {
+        presence: true,
+        format:{
+            pattern: "[a-z]+",
+            message: "can only contain a-z "
+        }
+    },
+    age: {
+        presence: true,
+        numericality: {
+            lessThan: 150
+        }
+    },
+    gender: {
+        presence: true,
+    },
+    email: {
+        presence: true,
+        email: true
+    },
+    phone: {
+        presence: true,
+        format:{
+            pattern: "[0-9]+",
+            message: "can only contain number(0-9) "
+        },
+        length: {
+            minimum : 10,
+            message: "must be at least 10 digits"
+        },        
+    },
+ 
+}
+
+const app =Vue.createApp({
+    data() {
+        return {
+            firstname: null,
+            lastname: null,
+            age: null,
+            gender: null,
+            email: null,
+            phone: null,
+            errors: null,
+        }
+    },
+    methods: {
+        checkForm(){
+            this.errors = validate({firstname: this.firstname,
+                                    lastname: this.lastname,
+                                    age: this.age,
+                                    gender: this.gender,
+                                    email: this.email,
+                                    phone: this.phone,
+                                    chosensubject: this.chosensubject},
+                                    constraints)
+            if(!this.errors){
+                alert("Your profile is updated successfully")
+            }
+        }
+    }
+})
+
+app.mount('#app')
+
+
